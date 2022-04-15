9f4ebb7d3137c133185f8f4f106503ce43c3e501
MaksKorkin <Maksuta62@mail.ru>
2022-04-15 13:49:24 +0300

    add dz

---
+++ dz3_task2.py
@@ -0,0 +1,27 @@
+def thesaurus(*args):
+    """ conver name list to dictionary like {A: [Alex..] , B:[Bob..]} """
+    out_dict = {}
+
+    for name in args:
+
+        if out_dict.get(name[0]):
+            out_dict[name[0]].append(name)
+        else:
+            out_dict[name[0]] = [name]
+
+    return out_dict
+
+
+def thesaurus_adv(*args):
+    """ conver name list to dictionary like second_name[0]:{name[0]: name + second_name} """
+    out_dict = {}
+    for elem in args:
+        name, second_name = elem.split()
+        if not out_dict.get(second_name[0]):
+            out_dict[second_name[0]] = { name[0] : [elem] }
+        elif not out_dict[second_name[0]].get(name[0]):
+            (out_dict[second_name[0]])[name[0]] = [elem]
+        else:
+            (out_dict[second_name[0]])[name[0]].append(elem)
+
+    return out_dict
